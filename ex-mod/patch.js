05784546606cf6c9618217880050c87666cf6539
WizardsBowl <Z123_Bluer223@outlook.com>
2026-02-24 03:02:05 +0800

    ex-mod: 创建示例插件

---
+++ ex-mod/patch.js
@@ -0,0 +1,10 @@
+{
+    "id": "ex-mod",
+    "servers": [
+        "https://raw.githubusercontent.com/WizardsBowl/wzb-thpatch/main/ex-mod/"
+    ],
+    "title": "Example Mod",
+    "dependencies": [
+        "nmlgc/base_tsa"
+    ]
+}
---
+++ repo.js
@@ -0,0 +1,11 @@
+{
+    "contact": "wizardsbowl233@outlook.com",
+    "id": "wzb-thpatch",
+    "patches": {
+        "ex-mod": "EX means Example."
+    },
+    "servers": [
+        "https://raw.githubusercontent.com/WizardsBowl/wzb-thpatch/main/"
+    ],
+    "title": "WizardsBowl's Touhou Patch Repository"
+}
