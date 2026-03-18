2d28bfe816a7c3544ff05bcf9fef4d77ffc4c198
WizardsBowl <125746317+WizardsBowl@users.noreply.github.com>
2026-03-19 00:29:30 +0800

    mc-hurt: 添加th14

---
+++ mc-hurt/patch.js
@@ -0,0 +1,10 @@
+{
+    "id": "mc-hurt",
+    "servers": [
+        "https://raw.githubusercontent.com/WizardsBowl/wzb-thpatch/main/mc-hurt/"
+    ],
+    "title": "Minecraft Hurt Sound Effect Patch",
+    "dependencies": [
+        "nmlgc/base_tsa"
+    ]
+}
--- repo.js
+++ repo.js
@@ -2,7 +2,8 @@
     "contact": "wizardsbowl233@outlook.com",
     "id": "wzb-thpatch",
     "patches": {
-        "ex-mod": "EX means Example."
+        "ex-mod": "EX means Example.",
+        "mc-hurt": "Minecraft hurt sound effect patch."
     },
     "servers": [
         "https://raw.githubusercontent.com/WizardsBowl/wzb-thpatch/main/"
