1e0a9eecdc40955c07bc2dd807f72884e07dac93
WizardsBowl <125746317+WizardsBowl@users.noreply.github.com>
2026-03-19 00:41:07 +0800

    repo: 更改repo.js的顺序 （这似乎是thcrap自动改的？？？）

--- repo.js
+++ repo.js
@@ -1,12 +1,12 @@
 {
-    "contact": "wizardsbowl233@outlook.com",
     "id": "wzb-thpatch",
-    "patches": {
-        "ex-mod": "EX means Example.",
-        "mc-hurt": "Minecraft hurt sound effect patch."
-    },
+    "title": "WizardsBowl's Touhou Patch Repository",
+    "contact": "wizardsbowl233@outlook.com",
     "servers": [
         "https://raw.githubusercontent.com/WizardsBowl/wzb-thpatch/main/"
     ],
-    "title": "WizardsBowl's Touhou Patch Repository"
+    "patches": {
+        "ex-mod": "EX means Example.",
+        "mc-hurt": "Minecraft hurt sound effect patch."
+    }
 }
